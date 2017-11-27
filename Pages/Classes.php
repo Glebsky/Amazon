4b8385cfb2b3ae05153adec6ebcd4b3f0effe386
Glebsky <lotamountofsoul@gmail.com>
2017-11-27 08:54:03 +0200

    DD

--- Pages/Classes.php
+++ Pages/Classes.php
@@ -19,7 +19,7 @@ class Curl
 		curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);
 	}
 
-	private function __destruct(){
+	public function __destruct(){
 		curl_close($this->ch);
 	}
 	public function set($name, $value){
@@ -29,13 +29,57 @@ class Curl
 	public function request($url){
 		curl_setopt($this->ch, CURLOPT_URL, $this->make_url($url));
 		$data = curl_exec($this->ch);
-		return $data;
+		return $this->process_result($data);
 	}
 	private function make_url($url){
 		if ($url[0] != '/') {
 			$url = '/'. $url;
 		}
 		return $this->host.$url;
+	}
+
+	public function process_result($data)
+	{
+		$p_n = "\n";
+		$p_rn = "\r\n";
+
+		$h_end_n = strpos($data, $p_n.$p_n);
+		$h_end_rn =  strpos($data, $p_rn.$p_rn);
+
+		$start = $h_end_n;
+		$p = $p_n;
+		if ($h_end_n === false || $h_end_rn < $h_end_n) {
+			$start = $h_end_rn;
+			$p = $p_rn;
+		}
+
+		$headers_part = substr($data, 0, $start);
+		$body_part = substr($data, $start + strlen($p)*2);
+		$lines = explode($p, $headers_part);
+
+		$headers = array();
+
+		$headers['start'] = $lines[0];
+
+		for ($i=1; $i < count($lines); $i++) { 
+			$del_pos = strpos($lines[$i], ':');
+			$name = substr($lines[$i], 0,$del_pos);
+			$value = substr($lines[$i], $del_pos+2);
+			$headers[$name] = $value;
+		}
+
+		return array(
+			'headers' => $headers,
+			'html' => $body_part
+		);
+
+	}
+
+	public function ssl($act)
+	{
+		curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, $act);
+		curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, $act);
+		return $this;
 	}
 }
  ?>
--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <?php 
 include_once('Pages/Classes.php');
 
+$post = array('login' => 'admin',
+	'pass' => '123456');
 
-$c = Curl::app('https://www.ebay.com/')->set(CURLOPT_HEADER, 1);
+$c = Curl::app('https://www.ebay.com')->set(CURLOPT_HEADER, 1)
+ ->set(CURLOPT_POST,true)
+ ->set(CURLOPT_POSTFIELDS,http_build_query($post))
+ ->set(CURLOPT_COOKIEJAR, $_SERVER['DOCUMENT_ROOT'].'/1.txt')
+ ->set(CURLOPT_COOKIEFILE,$_SERVER['DOCUMENT_ROOT'].'/1.txt')
+ ->ssl(0);
 $html = $c->request('/');
-echo $html;
+var_dump($html);
 
  ?>
