7fe0849f794b29b40b7efec8d1397e6e0b498120
Glebsky <lotamountofsoul@gmail.com>
2018-01-04 03:32:28 +0200

    Last Commit

--- Pages/Classes.php
+++ Pages/Classes.php
@@ -6,6 +6,10 @@ class Curl
 {
 	private $ch;
 	private $host;
+	private $options;
+	public $headers = array('Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+			'Accept-Encoding: gzip, deflate',
+			'Accept-Language: ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3');
 
 	public static function app($host){
 		return new self($host);
@@ -16,13 +20,29 @@ class Curl
 	{
 		$this->ch = curl_init();
 		$this->host = $host;
+		$this->options = array(CURLOPT_RETURNTRANSFER => true, CURLOPT_HTTPHEADER => array());
 		curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($this->ch, CURLOPT_HTTPHEADER, $this->headers);
+		$this->random_agent();
 	}
 
 	public function __destruct(){
 		curl_close($this->ch);
 	}
+	/**
+	 * Creates headers
+	 * @param True or False
+	 * @return Curl Class
+	 * @author 
+	 **/
+	public function headers($act)
+	{
+		$this->set(CURLOPT_HEADER,$act);
+		return $this;
+	}
+
 	public function set($name, $value){
+		$this->options[$name] = $value;
 		curl_setopt($this->ch, $name, $value);
 		return $this;
 	}
@@ -38,47 +58,151 @@ class Curl
 		return $this->host.$url;
 	}
 
-	public function process_result($data)
+	public function add_header($header)
 	{
-		$p_n = "\n";
-		$p_rn = "\r\n";
+		$this->options[CURLOPT_HTTPHEADER][] = $header;
+		$this->set(CURLOPT_HTTPHEADER, $this->options[CURLOPT_HTTPHEADER]);
+		return $this;
+	}
 
-		$h_end_n = strpos($data, $p_n.$p_n);
-		$h_end_rn =  strpos($data, $p_rn.$p_rn);
+	public function add_headers($headers)
+	{
+		foreach ($headers as $h)
+			$this->options[CURLOPT_HTTPHEADER][] = $h;
 
-		$start = $h_end_n;
-		$p = $p_n;
-		if ($h_end_n === false || $h_end_rn < $h_end_n) {
-			$start = $h_end_rn;
-			$p = $p_rn;
+		$this->set(CURLOPT_HTTPHEADER, $this->options[CURLOPT_HTTPHEADER]);
+		return $this;
+	}
+
+	public function clear_headers()
+	{
+		$this->options[CURLOPT_HTTPHEADER] = array();
+		$this->set(CURLOPT_HTTPHEADER,$this->options[CURLOPT_HTTPHEADER]);
+		return $this;
+	}
+
+	public function follow($param)
+	{
+		$this->set(CURLOPT_FOLLOWLOCATION,$param);
+		return $this;
+	}
+
+	public function referer($param)
+	{
+		$this->set(CURLOPT_REFERER,$param);
+		return $this;
+	}
+
+	public function user_agent($agent)
+	{
+		$this->set(CURLOPT_USERAGENT,$agent);
+		return $this;
+	}
+
+	public function config_load($file)
+	{
+		$data = file_get_contents($file);
+		$data = unserialize($data);
+		curl_setopt_array($this->ch, $data);
+
+		foreach ($data as $key => $value) {
+			$this->options[$key] = $value;
 		}
+		return $this;
+	}
 
-		$headers_part = substr($data, 0, $start);
-		$body_part = substr($data, $start + strlen($p)*2);
-		$lines = explode($p, $headers_part);
+	public function config_save($file)
+	{
+		$data = serialize($this->options);
+		file_put_contents($file, $data);
+		return $this;
+	}
 
-		$headers = array();
+	public function random_userAgent($agent)
+	{
+		
+	}
 
-		$headers['start'] = $lines[0];
+	private function process_result($data)
+	{
+		if (isset($this->options[CURLOPT_HEADER]) && $this->options[CURLOPT_HEADER]) {
 
-		for ($i=1; $i < count($lines); $i++) { 
-			$del_pos = strpos($lines[$i], ':');
-			$name = substr($lines[$i], 0,$del_pos);
-			$value = substr($lines[$i], $del_pos+2);
-			$headers[$name] = $value;
-		}
+			$info = curl_getinfo($this->ch);
 
-		return array(
+			$headers_part = trim(substr($data, 0, $info['header_size']));
+			$body_part = substr($data, $info['header_size']);
+
+			$headers_part = str_replace("\r\n", "\n", $headers_part);
+			$headers = str_replace("\r", "\n", $headers_part);
+
+			$headers = explode("\n\n", $headers);
+			$headers_part = end($headers);
+
+			$lines = explode("\n", $headers_part);
+			$headers = array();
+
+			$headers['start'] = $lines[0];
+
+			for ($i=1; $i < count($lines); $i++) { 
+				$del_pos = strpos($lines[$i], ':');
+				$name = substr($lines[$i], 0, $del_pos);
+				$value = substr($lines[$i], $del_pos + 2);
+				$headers[$name] = $value;
+			}
+
+			return array(
 			'headers' => $headers,
 			'html' => $body_part
 		);
+		}else{
+			return array(
+			'headers' => array(),
+			'html' => $data
+		);
+		}
 
 	}
 
-	public function ssl($act)
+	public function ssl($act=1)
 	{
-		curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, $act);
-		curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, $act);
+		try {
+		$this->set(CURLOPT_SSL_VERIFYPEER, $act);
+		$this->set(CURLOPT_SSL_VERIFYPEER, $act);
+		return $this;
+		} catch (Exception $e) {
+			die($e->getMessage());
+		}
+	}
+
+	public function post($arr)
+	{	try {
+		if ($arr === false) {
+			$this->set(CURLOPT_POST,false);
+			return $this;
+		}
+		$this->set(CURLOPT_POST, true);
+		$this->set(CURLOPT_POSTFIELDS, http_build_query($arr));
+		return $this;
+	} catch (Exception $e) {
+		die($e->getMessage());
+	}
+	}
+
+	public function cookie($cookie_name)
+	{
+		try {
+		$this->set(CURLOPT_COOKIEJAR, $_SERVER['DOCUMENT_ROOT'].'/cookie/'.$cookie_name.'.cki');
+		$this->set(CURLOPT_COOKIEFILE, $_SERVER['DOCUMENT_ROOT'].'/cookie/'.$cookie_name.'.cki');
+		return $this;
+		} catch (Exception $e) {
+			die($e->getMessage());
+		}
+	}
+
+	public function random_agent(){
+		// https://udger.com/resources/ua-list - больше агентов тут
+		$data = file("config/agents.txt");
+		$this->user_agent(trim($data[rand(0, count($data) - 1)]));
 		return $this;
 	}
 }
--- index.php
+++ index.php
@@ -1,16 +1,27 @@
 <?php 
 include_once('Pages/Classes.php');
+include_once('simplehtmldom/simple_html_dom.php');
 
-$post = array('login' => 'admin',
-	'pass' => '123456');
+$c = Curl::app('https://www.ebay.com')
+ ->headers(1)
+ ->referer('google.com')
+ ->ssl(0)
+ ->add_header('accept:application/json; charset=utf-8; profile="https://www.mediawiki.org/wiki/Specs/Summary/1.2.0"')
+ ->add_header('accept-language:ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7')
+ ->random_agent()
+ ->cookie('ebay');
+ $c->config_save('wiki.cfg');
+$html = $c->request('/sch/Kitchen-Dining-Bar-/20625/i.html?_pgn=2&_ipg=200');
+echo $html['html'];
+$pHtml = str_get_html($html['html']);
+$title = $pHtml->find('.lvtitle');
 
-$c = Curl::app('https://www.ebay.com')->set(CURLOPT_HEADER, 1)
- ->set(CURLOPT_POST,true)
- ->set(CURLOPT_POSTFIELDS,http_build_query($post))
- ->set(CURLOPT_COOKIEJAR, $_SERVER['DOCUMENT_ROOT'].'/1.txt')
- ->set(CURLOPT_COOKIEFILE,$_SERVER['DOCUMENT_ROOT'].'/1.txt')
- ->ssl(0);
-$html = $c->request('/');
-var_dump($html);
+foreach ($title as $t) {
+	echo $t->plaintext.'<br>';
+}
+
+$pHtml->clear();
+unset($pHtml);
+
 
  ?>
