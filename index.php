59a84f78a5aa54dbb290d433b934e34a297d82df
Glebsky <lotamountofsoul@gmail.com>
2018-03-07 11:41:27 +0200

    Amazon
    Add Second Shop

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 include_once('Pages/Classes.php');
 include_once('simplehtmldom/simple_html_dom.php');
 
-$c = Curl::app('https://www.ebay.com')
+$ebay = Curl::app('https://www.ebay.com')
  ->headers(1)
  ->referer('google.com')
  ->ssl(0)
@@ -10,18 +10,11 @@ $c = Curl::app('https://www.ebay.com')
  ->add_header('accept-language:ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7')
  ->random_agent()
  ->cookie('ebay');
- $c->config_save('wiki.cfg');
-$html = $c->request('/sch/Kitchen-Dining-Bar-/20625/i.html?_pgn=2&_ipg=200');
-echo $html['html'];
-$pHtml = str_get_html($html['html']);
-$title = $pHtml->find('.lvtitle');
+ $ebay->config_save('wiki.cfg');
+$html = $ebay->request('/sch/Kitchen-Dining-Bar-/20625/i.html?_pgn=2&_ipg=200');
 
-foreach ($title as $t) {
-	echo $t->plaintext.'<br>';
-}
 
-$pHtml->clear();
-unset($pHtml);
+
 
 
  ?>
